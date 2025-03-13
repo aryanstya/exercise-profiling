2d4ab14c372e6d97a1b5e1e0582f63022cd7f5c1
Aryan Primasatya <aryanstya238@gmail.com>
2025-03-13 18:02:51 +0700

    [Refactoring] - Optimizing method getAllStudentWithCourse

--- src/main/java/com/advpro/profiling/tutorial/TutorialApplication.java
+++ src/main/java/com/advpro/profiling/tutorial/TutorialApplication.java
@@ -1,5 +1,6 @@
 package com.advpro.profiling.tutorial;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TutorialApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TutorialApplication.class, args);
+		SpringApplication app = new SpringApplication(TutorialApplication.class);
+		app.setBannerMode(Banner.Mode.OFF);
+		app.run(args);
 	}
-
 }
